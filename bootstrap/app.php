e6b0a7f79aa84cd507faea33d914f56d5da62bcd
mohamed-arab1 <momenmoslem991@gmail.com>
2026-02-23 00:42:02 +0200

    fix(vercel): correctly handle the $app configuration for Vercel's tmp storage

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Auth\AuthenticationException;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
