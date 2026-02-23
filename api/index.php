99859f7c2023c80559a883cddd409904e4ab30e7
mohamed-arab1 <momenmoslem991@gmail.com>
2026-02-23 23:19:50 +0200

    debug: check for vendor/autoload on vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    header('Content-Type: application/json');
+    echo json_encode(['error' => 'Vendor autoload is missing. Composer install did not run on Vercel.']);
+    exit;
+}
 
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
