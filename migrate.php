a0f6a74de2148c8873e209755a56e1486db12db5
mohamed-arab1 <momenmoslem991@gmail.com>
2026-02-23 23:36:05 +0200

    chore: remove debug scripts now that Vercel configuration is successfully rendering 200s and serving the app

--- migrate.php
+++
@@ -1,6 +0,0 @@
-<?php
-require "vendor/autoload.php";
-$app = require_once "bootstrap/app.php";
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-\Illuminate\Support\Facades\Artisan::call("migrate", ["--force" => true]);
-echo \Illuminate\Support\Facades\Artisan::output();
